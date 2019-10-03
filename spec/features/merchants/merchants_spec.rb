3430b5c9a9c7fb4cf68236e68b6c2e5f722a0e78
Tyla Devon <tyladevon@gmail.com>
2019-10-03 14:39:40 -0600

    Create test and complete user story 2

--- spec/features/merchants/merchants_spec.rb
+++ spec/features/merchants/merchants_spec.rb
@@ -13,11 +13,32 @@ RSpec.describe "merchants index page", type: :feature do
                                  state: "CO",
                                  zip: 80304)
 
-
-
   visit "/merchants"
 
   expect(page).to have_content(merchant_1.name)
   expect(page).to have_content(merchant_2.name)
+  end
+end
+
+RSpec.describe "merchant page", type: :feature do
+  it "can see merchant by id" do
+    merchant_1 = Merchant.create(name: "Seeds and Flowers",
+                                 address: "1234 Mountain Rd",
+                                 city:  "Boulder",
+                                 state: "CO",
+                                 zip: 80304)
+    merchant_2 = Merchant.create(name: "Everything Dogs",
+                                 address: "5678 Lake Dr",
+                                 city:  "Boulder",
+                                 state: "CO",
+                                 zip: 80304)
+
+  visit "/merchants/#{merchant_1.id}"
+
+  expect(page).to have_content(merchant_1.name)
+  expect(page).to have_content(merchant_1.address)
+  expect(page).to have_content(merchant_1.city)
+  expect(page).to have_content(merchant_2.state)
+  expect(page).to have_content(merchant_2.zip)
   end
 end
---
+++ spec/models/merchent_spec.rb
@@ -0,0 +1,23 @@
+# require 'rails_helper'
+#
+# describe "", type: :model do
+#   it "" do
+#     merchant_1 = Merchant.create(name: "Seeds and Flowers",
+#                                  address: "1234 Mountain Rd",
+#                                  city:  "Boulder",
+#                                  state: "CO",
+#                                  zip: 80304)
+#     merchant_2 = Merchant.create(name: "Everything Dogs",
+#                                  address: "5678 Lake Dr",
+#                                  city:  "Boulder",
+#                                  state: "CO",
+#                                  zip: 80304)
+#
+#
+#
+#   visit "/merchants"
+#
+#   expect(page).to have_content(merchant_1.name)
+#   expect(page).to have_content(merchant_2.name)
+#   end
+# end
