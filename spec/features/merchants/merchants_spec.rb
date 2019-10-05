bc68c50b043584d9f632a1526ff15fc6285b2c1a
Tyla Devon <tyladevon@gmail.com>
2019-10-05 07:41:18 -0600

    Add test for user story 4, Merchant Create. Add path, action item and view for Merchant Create.

--- spec/features/merchants/merchants_spec.rb
+++ spec/features/merchants/merchants_spec.rb
@@ -42,3 +42,31 @@ RSpec.describe "merchant page", type: :feature do
   expect(page).to have_content(merchant_2.zip)
   end
 end
+
+RSpec.describe "merchant page", type: :feature do
+  it "can see link for new merchant" do
+    merchant_1 = Merchant.create(name: "Seeds and Flowers",
+                                 address: "1234 Mountain Rd",
+                                 city:  "Boulder",
+                                 state: "CO",
+                                 zip: 80304)
+    merchant_2 = Merchant.create(name: "Everything Dogs",
+                                 address: "5678 Lake Dr",
+                                 city:  "Boulder",
+                                 state: "CO",
+                                 zip: 80304)
+
+  visit "/merchants"
+  click_link "New Merchant"
+  expect(current_path).to eq("/merchants/new")
+  fill_in "Name", with: "Cats and Stuff"
+  fill_in "Address", with: "34543 Something St."
+  fill_in "City", with: "Boulder"
+  fill_in "State", with: "CO"
+  fill_in "Zip", with: "80302"
+  click_button "Create Merchant"
+
+  expect(current_path).to eq("/merchants")
+  expect(page).to have_content("Cats and Stuff")
+  end
+end
