707e516291be2d8aac2ce93ecabd017aff3ba25b
Tyla Devon <tyladevon@gmail.com>
2019-10-06 20:09:40 -0600

    Add validation for Merchant and Item class. Add relationship between the two.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
-
+  validates_presence_of :name, :description, :price, :image, :status, :inventory, :name_of_merchant
+  belongs_to :merchant
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,4 @@
 class Merchant < ApplicationRecord
-
-end 
+  validates_presence_of :name, :address, :city, :state, :zip
+  has_many :items
+end
